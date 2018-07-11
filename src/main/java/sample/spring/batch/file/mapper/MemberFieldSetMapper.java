2d17e4f492d9ef4080c5fd37c177bc144c15f254
Kengo Mukai <kengo@sample.com>
2018-07-11 16:42:17 +0900

    ファイルの自動テストサンプルを作成

---
+++ src/main/java/sample/spring/batch/file/mapper/MemberFieldSetMapper.java
@@ -0,0 +1,21 @@
+package sample.spring.batch.file.mapper;
+
+import org.springframework.batch.item.file.mapping.FieldSetMapper;
+import org.springframework.batch.item.file.transform.FieldSet;
+import org.springframework.validation.BindException;
+
+import sample.spring.batch.file.dto.Member;
+
+public class MemberFieldSetMapper implements FieldSetMapper<Member> {
+
+	@Override
+	public Member mapFieldSet(FieldSet fieldSet) throws BindException {
+		Member member = new Member();
+
+		member.setNum(fieldSet.readInt("num", 0));
+		member.setName(fieldSet.readString("name"));
+
+		return member;
+	}
+
+}
