2bfa1ad850c187caa8d5a96472f723ebc094667f
Kengo Mukai <kengo@sample.com>
2018-07-15 14:52:08 +0900

    DBUnitを用いたテストの追加

--- src/test/java/sample/spring/batch/logic/file/FileJobTest.java
+++ src/test/java/sample/spring/batch/logic/file/FileJobTest.java
@@ -2,12 +2,7 @@ package sample.spring.batch.logic.file;
 
 import static org.junit.Assert.*;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
@@ -16,34 +11,18 @@ import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import sample.spring.batch.util.SpringBatchTestSupport;
 
 @ContextConfiguration(locations = { "/module-context_file.xml", "/test-context.xml" })
-@RunWith(SpringJUnit4ClassRunner.class)
-public class FileJobTest {
+public class FileJobTest extends SpringBatchTestSupport {
 
 	@Autowired
 	private JobLauncherTestUtils jobLauncherTestUtils;
 
-	private static final String INPUT_FILE = "./src/test/resources/file/input.csv";
-	private static final String OUTPUT_FILE = "./src/test/resources/file/output.csv";
-	private static final String EXPECTED_FILE = "./src/test/resources/file/output_expected.csv";
-
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
+	private static final String INPUT_FILE = TEST_FILE_PATH + "input.csv";
+	private static final String OUTPUT_FILE = TEST_FILE_PATH + "output.csv";
+	private static final String EXPECTED_FILE = TEST_FILE_PATH + "output_expected.csv";
 
 	@Test
 	public void test() throws Exception {
