5c6b266f88a85480833c94684ff310620164599b
Kengo Mukai <kengo@sample.com>
2018-07-15 10:27:43 +0900

    JobRepositoryをキャッシュからPostgresに変更

---
+++ misc/sql/job-repository/ClearJobRepository.sql
@@ -0,0 +1,7 @@
+TRUNCATE batch_job_execution CASCADE;
+TRUNCATE batch_job_execution_context CASCADE;
+TRUNCATE batch_job_execution_params CASCADE;
+TRUNCATE batch_job_instance CASCADE;
+TRUNCATE batch_step_execution CASCADE;
+TRUNCATE batch_step_execution_context CASCADE;
+COMMIT;
---
+++ misc/sql/job-repository/DropJobRepository.sql
@@ -0,0 +1,7 @@
+DROP TABLE batch_job_execution CASCADE;
+DROP TABLE batch_job_execution_context CASCADE;
+DROP TABLE batch_job_execution_params CASCADE;
+DROP TABLE batch_job_instance CASCADE;
+DROP TABLE batch_step_execution CASCADE;
+DROP TABLE batch_step_execution_context CASCADE;
+COMMIT;
